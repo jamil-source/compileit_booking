661677ec9a95217d3ae0b465cdee3b6bd744e8cb
Jamil Bariche <jamil.bariche@iver.se>
2024-11-15 11:36:12 +0100

    #3 create shared-btn. Add styling for home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { SharedBtnComponent } from './shared/components/shared-btn/shared-btn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SharedBtnComponent
   ],
   imports: [
     BrowserModule,
